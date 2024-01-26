c9e8bd954a2a5a7c0dd6e5d6cf8e691643749db1
Andrés Martínez <autummata@gmail.com>
2024-01-25 19:39:04 -0700

    Deleted .DS_Store

--- utils/initdb.py
+++ utils/initdb.py
@@ -19,17 +19,3 @@ def init_db():
     )
     conn_users.commit()
     conn_users.close()
-    
-
-def create_table_from_sql(conn, sql_file_path):
-    # Execute SQL statements from the specified file
-    with open(sql_file_path, "r") as sql_file:
-        sql_statements = sql_file.read()
-        conn.executescript(sql_statements)
-
-
-def does_table_exist(conn, table_name):
-    # Check if the table exists in the database
-    query = f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'"
-    result = conn.execute(query).fetchone()
-    return result is not None
